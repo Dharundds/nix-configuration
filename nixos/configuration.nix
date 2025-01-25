bd66caca5d778b46507222c48465fa7dbc9ad99b
dharundds <dharundds@gmail.com>
2025-01-25 11:18:00 +0530

    discord

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,6 +113,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      vscode
+     discord
      git
      gh
      brave
