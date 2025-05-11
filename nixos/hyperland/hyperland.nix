9897661e624b66b99a3db942ca72bceca68aa6b8
installer <installer@gmail.com>
2025-05-11 13:59:25 +0530

    hyperland add

--- nixos/hyperland/hyperland.nix
+++ nixos/hyperland/hyperland.nix
@@ -2,14 +2,14 @@
 {
   programs.hyprland = {
     enable = true;
-    # nvidiaPatches = true;
-    # xwayland.enable = true;
+    nvidiaPatches = true;
+    xwayland.enable = true;
   };
 
-  # environment.sessionVariables = {
-  #   # If your cursor becomes invisible
-  #   WLR_NO_HARDWARE_CURSORS = "1";
-  #   # Hint electron apps to use wayland
-  #   NIXOS_OZONE_WL = "1";
-  # };
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
 }
