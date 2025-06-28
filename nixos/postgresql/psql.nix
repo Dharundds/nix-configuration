575145201ce216edaa694d1d74b145626c32387a
installer <installer@gmail.com>
2025-06-28 21:00:42 +0530

    psql

--- nixos/postgresql/psql.nix
+++ nixos/postgresql/psql.nix
@@ -2,5 +2,16 @@
 {
   config.services.postgresql = {
     enable = true;
+    package = pkgs.postgresql_16;
+    enableTCPIP=true;
+    authentication = pkgs.lib.mkOverride 10 ''
+    #...
+    #type database  DBuser  auth-method
+    local all       all     trust
+    # ipv4
+    host  all      all     127.0.0.1/32   md5
+    # ipv6
+    host all       all     ::1/128        md5
+  '';
   };
 }
