b4974cc3f82dde2e3942e8a0b78400eb7a5164ba
installer <installer@gmail.com>
2025-07-07 08:15:46 +0530

    network rule for tcp allow

---
+++ nixos/networking/networking.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, modulesPath, ... }:
+{
+
+  # networking.firewall.enable = true;
+  # networking.firewall.allowPing = true;
+
+  networking.firewall.allowedTCPPorts = [ 80,8080,3000 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+}
