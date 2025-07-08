383fb9dae128b8f4d1211cb2fbb5ae26a1cf576a
installer <installer@gmail.com>
2025-07-08 10:06:53 +0530

    UPDATE[network]: allowudp port 1900 for media streaming

--- nixos/networking/networking.nix
+++ nixos/networking/networking.nix
@@ -4,8 +4,8 @@
   # networking.firewall.enable = true;
   # networking.firewall.allowPing = true;
 
-  networking.firewall.allowedTCPPorts = [ 80,8080,3000 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 80 8080 3000 ];
+  networking.firewall.allowedUDPPorts = [ 1900 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
