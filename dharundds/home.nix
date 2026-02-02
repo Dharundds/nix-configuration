c51fe3a8e1eb90a43c7b467bff9bc061f4fa50d7
installer <installer@gmail.com>
2026-02-02 09:24:50 +0530

    home state version to 25.11

--- dharundds/home.nix
+++ dharundds/home.nix
@@ -33,7 +33,7 @@
   home.username = "dharundds";
   home.homeDirectory = "/home/dharundds";
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
     dconf-editor
